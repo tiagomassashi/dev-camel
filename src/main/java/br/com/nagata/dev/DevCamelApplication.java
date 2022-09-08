cbbef558f83414921825c13ff5e6144a55fba308
Tiago Massashi Nagata <59301876+tiagomassashi@users.noreply.github.com>
2022-09-08 19:29:40 -0300

    first commit

---
+++ src/main/java/br/com/nagata/dev/DevCamelApplication.java
@@ -0,0 +1,13 @@
+package br.com.nagata.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevCamelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevCamelApplication.class, args);
+	}
+
+}
